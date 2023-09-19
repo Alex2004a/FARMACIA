d38179857ce29545b8ee9f9671ab6f53b20d5a28
Alex2004a <alexipanaque091@gmail.com>
2023-09-19 17:42:50 -0500

    termine mi formulario: Menu_IU, TipoUsuario_IU, Turno_IU, Usuario

---
+++ src/CapaConexion/Conexion.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CapaConexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author labor
+ */
+public class Conexion {
+
+    private static String clase = "com.mysql.cj.jdbc.Driver";
+
+    private static String url = "jdbc:mysql://localhost:3306/farmacia?serverTimezone=UTC";
+    private static String usuario = "root";
+    private static String clave = "123456";
+
+    public Conexion() {
+    }
+
+    public Connection conectar() {
+        Connection link = null;
+        try {
+
+            link = DriverManager.getConnection(url, usuario, clave);
+
+        } catch (SQLException e) {
+
+            JOptionPane.showConfirmDialog(null, e);
+        }
+
+        return link;
+
+    }
+}
